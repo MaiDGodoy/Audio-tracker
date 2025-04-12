09368d6b03412245436b3c6ea560304cfc6f03cd
MaiDGodoy <132632001+MaiDGodoy@users.noreply.github.com>
2025-04-12 16:01:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -143,7 +143,18 @@ function initPlayer() {
   sound.play();
 }
 
-
+// sw.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('audio-cache').then((cache) => {
+      return cache.addAll([
+        // Lista de URLs de audio
+        'https://www.dropbox.com/scl/fi/jg9nx593hejrn19ocp04e/Realm-of-Tranquil-Eternity-Disc-1_-Sakura-and-Violet-ThunderGenshin-Impact.mp3?rlkey=eeac442as89tf6mvmlmuw8ulm&raw=1',
+        'https://www.dropbox.com/scl/fi/n0ymferge9xacyx7bwrkx/Isabella-s-Lullaby-vocal-and-mandolin-ver.-extended.mp3?rlkey=bxk52om38bzhhewmkiq7kja1x&st=izlkdnp9&dl=1'
+      ]);
+    })
+  );
+});
   
   function updatePlayerTitle() {
     const currentTrackData = playlist[currentTrack];
